a0ad1f08f630550fd9ca47a82483854f673ecb38
SairaMadrid <sairakbar@gmail.com>
2023-07-29 18:50:14 +0200

    Database migration, hotels list component changed

--- client/src/components/HotelsList.jsx
+++ client/src/components/HotelsList.jsx
@@ -10,11 +10,20 @@ function HotelsList() {
   const getHotels = async () => {
     try {
       const response = await fetch("http://localhost:4000/api/hotels");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
       const jsonData = await response.json();
       setHotels(jsonData);
     } catch (error) {
-      console.error(err.message);
+      console.error("Error fetching hotels:", error.message);
     }
+  };
+
+  // Assuming you have a `deleteHotels` function defined for deleting hotels.
+  const deleteHotels = async (hotelId) => {
+    // Implement the logic to delete the hotel with the specified ID.
+    // Make sure to update the `hotels` state to reflect the changes.
   };
 
   return (
@@ -29,6 +38,7 @@ function HotelsList() {
             src={hotel.image_URL}
             className="card-img-top rounded"
             style={{ aspectRatio: "1/1", objectFit: "cover" }}
+            alt={hotel.name} // Add an alt attribute for accessibility
           />
           <div className="card-body">
             <p className="card-title">{hotel.name}</p>
