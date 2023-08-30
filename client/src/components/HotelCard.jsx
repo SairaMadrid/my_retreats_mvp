2989b4185edd2f306142c94a51552e777359e82d
SairaMadrid <sairakbar@gmail.com>
2023-08-30 18:55:51 +0200

    Description data added, search results styled, favourites hearts added

--- client/src/components/HotelCard.jsx
+++ client/src/components/HotelCard.jsx
@@ -1,18 +1,43 @@
-import React from "react"
+import React, { useState } from "react"
 
 const HotelCard = ({ hotel }) => {
+  const [isFavorite, setIsFavorite] = useState(false)
+
+  const toggleFavorite = () => {
+    setIsFavorite((prevState) => !prevState)
+  }
+
   return (
-    <div className="col-lg-4 col-md-6 col-sm-12 mb-4">
-      <div className="card h-100">
-        {" "}
-        {/* Use h-100 to ensure equal card height */}
+    <div className="card h-100 hotel-card">
+      <div style={{ position: "relative" }}>
+        {/* <div className="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div className="card h-100"> */}
         <img
           src={hotel.image_URL}
           className="card-img-top rounded"
           alt={hotel.name} // Add an alt attribute for accessibility
+          style={{ maxHeight: "200px", objectFit: "cover" }}
         />
+        <div
+          style={{
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            zIndex: 1,
+          }}
+        >
+          <div
+            className={`heart-icon ${isFavorite ? "favorite" : ""}`}
+            onClick={toggleFavorite}
+          >
+            <i
+              className="fa-solid fa-heart"
+              style={{ color: isFavorite ? "red" : "white" }}
+            ></i>
+          </div>
+        </div>
         <div className="card-body">
-          <p className="card-title">{hotel.name}</p>
+          {/* <p className="card-title">{hotel.name}</p> */}
           <p className="card-text">Location: {hotel.location}</p>
           <p className="card-text">Price: {hotel.price_range}</p>
           <p className="card-text">
@@ -31,11 +56,16 @@ const HotelCard = ({ hotel }) => {
               <i className="fas fa-times-circle text-danger"></i>
             )}
           </p>
-          <div className="container text-center">
-            <div>
-              <i className="fa-solid fa-heart"></i>
-            </div>
-          </div>
+          <a
+            href="#!"
+            className="read-more"
+            onClick={(e) => {
+              e.preventDefault() // Prevent default link behavior
+              // Handle read more logic here if needed
+            }}
+          >
+            Read More
+          </a>
         </div>
       </div>
     </div>
--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -2,14 +2,43 @@ import React, { useState } from "react"
 import HotelCard from "./HotelCard"
 
 const SearchResults = ({ results }) => {
+  const [selectedHotel, setSelectedHotel] = useState(null)
+
+  const toggleDescription = (hotel) => {
+    setSelectedHotel(selectedHotel === hotel ? null : hotel)
+  }
+
   return (
     <div className="container">
       <h2 className="mt-4 mb-3">Search Results</h2>
       <div className="row">
         {results.length > 0 ? (
           results.map((hotel) => (
-            <div key={hotel.id} className="col-md-4 mb-3">
-              <HotelCard hotel={hotel} />
+            <div key={hotel.id} className="col-lg-4 col-md-6">
+              <div className="hotel-card">
+                <h5
+                  className="hotel-name-link"
+                  onClick={() => toggleDescription(hotel)}
+                >
+                  <a href="#!">{hotel.name}</a>
+                </h5>
+                <HotelCard hotel={hotel} />
+                {selectedHotel === hotel && (
+                  <div className="description-overlay">
+                    <div className="modal-container">
+                      <div className="hotel-description">
+                        {hotel.description}
+                        <button
+                          className="close-button"
+                          onClick={() => toggleDescription(hotel)}
+                        >
+                          Close
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
             </div>
           ))
         ) : (
